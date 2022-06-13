69728c7bf4461e61e71062db4be18470f213f455
Meir Borowski <72465933+meirborowski@users.noreply.github.com>
2022-06-13 13:01:13 +0300

    updated cards title size

---
+++ educationScripts.js
@@ -0,0 +1,11 @@
+
+function launchSmileAndLearnApp() {
+    const url = "intent://clever.khanacademykids.org/#Intent;scheme=https;package=org.khankids.android;end";
+
+    if (navigator.userAgent.toLowerCase().indexOf('android') > -1) {
+        window.location.href = url;
+    }
+    else {
+        alert("You must be using an Android device to launch the app.");
+    }
+}
--- homeScripts.js
+++ homeScripts.js
@@ -9,3 +9,15 @@ function launchKhanApp() {
         alert("You must be using an Android device to launch the app.");
     }
 }
+
+
+function launchODKApp() {
+    const url = "intent://clever.khanacademykids.org/#Intent;scheme=https;package=org.odk.collect.android;end";
+
+    if (navigator.userAgent.toLowerCase().indexOf('android') > -1) {
+        window.location.href = url;
+    }
+    else {
+        alert("You must be using an Android device to launch the app.");
+    }
+}
